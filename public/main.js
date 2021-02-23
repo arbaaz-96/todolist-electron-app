41cce9d96856985483755bf9c9c840e6c995ec1b
arbaaz-96 <60695628+arbaaz-96@users.noreply.github.com>
2021-02-23 15:36:49 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -2,33 +2,15 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev')
 
-/*function createWindow() {
-   const mainWindow = new BrowserWindow({ width: 1100, height: 680 });
-    mainWindow.loadURL('http://localhost:3000');
-        
-    //mainWindow.on("closed", () => (mainWindow = null));
-}*/
-
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
       width: 1100,
       height: 680
-      /*webPreferences: {
-        nodeIntegration: true,
-      },*/
     })
   
     mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
-      /*isDev
-        ? 'http://localhost:3000'
-        : `file://${path.join(__dirname, '../build/index.html')}`,
-    )*/
-  
-    /*mainWindow.on('closed', () => {
-      mainWindow = null
-    })*/
   }
 
 app.on("ready", createWindow);
